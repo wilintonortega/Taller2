948193217d31d72857a81828b6a1c3e880674e17
wili1991 <wikki9106@gmail.com>
2023-02-13 22:21:02 -0500

    enunciado segundo ej

--- condicionales_Edad_Switch_Case_13_02_2023/main.js
+++ condicionales_Edad_Switch_Case_13_02_2023/main.js
@@ -1,3 +1,5 @@
+//2.	Haz un programa de nombre main.js y otro llamado index.html incluir el archivo JavaScript en el <head></ head>, que has de grabar en una carpeta llamada condicionales_Edad_Switch_Case_FechaActual, por medio del prompt pedir la edad y verificar si la edad ingresada es de 18 mostrar que “Es mayor de edad” del caso contrario mostrar un mensaje por medio de la consola o de la ventana emergente que diga “La edad ingresada no cumple con los parámetros “, usando el condicional Switch Case.
+
 let edad=Number(prompt("Ingrese la edad"));
 
 switch (edad) {
